b09e8abeaf8ed4e6eba76caf8769b4da6dd68d87
elinaborgstrom <elho2406@student.miun.se>
2024-10-09 14:26:17 +0200

    Gör "lägg till-knapen" disabled som startfuntion. Lägger till en if och else-sats med vilkor knappen. Meddelande med text vid inkorrekt inmatning.

--- js/main.js
+++ js/main.js
@@ -22,6 +22,11 @@ function init () {
 
     console.log("initierar.."); //console.log för att testköra att allt fungerar
 
+      //inaktivera lägg till-knapp 
+      newTodoButtonEl.disabled = true; 
+
+ 
+
     //läser in todo-lista 
     loadTodoList() 
 
@@ -31,6 +36,28 @@ function init () {
 function checkInput() { 
 
     console.log("kontrollerar input.."); 
+
+    let input = newTodoEl.value; 
+
+ 
+
+//kontrollera om inmatad text har korrekt antal tecken 
+
+if (input.length > 4 ) { 
+
+    //inget meddelande om korrekt input 
+    messgeEl.innerHTML = ""; 
+
+    newTodoButtonEl.disabled = false; 
+
+} else { 
+
+    //meddelande med text vid för få tecken 
+    messgeEl.innerHTML = "Måste innehålla minst fem tecken."; 
+
+    newTodoButtonEl.disabled = true; 
+
+} 
     
 } 
 
